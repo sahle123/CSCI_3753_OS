0ccef93ca0c3e754d0e1b39bcd7b5e58957f395c
Sal Alturaigi <saal4106@gmail.com>
2014-02-21 15:59:29 -0700

    all setup

---
+++ PA2/multi-lookup.c
@@ -0,0 +1,8 @@
+/*	multi-lookup.c
+*
+*	Author: Sahle A. "Nomad" Alturaigi
+*	Date:   Feb. 21, 2014
+*	
+*	Programming assignment 2 implementation file
+*
+*/
---
+++ PA2/multi-lookup.h
@@ -0,0 +1,8 @@
+/*	multi-lookup.h
+*
+*	Author: Sahle A. "Nomad" Alturaigi
+*	Date:   Feb. 21, 2014
+*	
+*	Programming assignment 2 header file
+*
+*/
